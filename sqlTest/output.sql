28d29432dd5d3cf321ab50b58fa49707821be794
kaishern <leek0125@e.ntu.edu.sg>
2022-03-06 21:59:47 +0800

    Add SQL code for verification

--- sqlTest/output.sql
+++ sqlTest/output.sql
@@ -1,26 +1,26 @@
-CREATE TABLE sqlOutput AS 
+CREATE TABLE sqlOutput AS
 WITH tempT as
-	(SELECT SUBSTR(Timestamp,0, 8) month, MAX(humidity) maxH, MAX(temperature) maxT, MIN(humidity) minH, MIN(temperature) minT
-		FROM year02_1_31
-		WHERE timestamp LIKE "2002%" AND station = "Changi"
+	(SELECT SUBSTR(Timestamp,0, 8) month, station, MAX(humidity) maxH, MAX(temperature) maxT, MIN(humidity) minH, MIN(temperature) minT
+		FROM SingaporeWeather_1
+		WHERE (timestamp LIKE "2003%" OR timestamp LIKE "2013%") AND station = "Paya Lebar"
         GROUP BY month)
-SELECT DISTINCT(SUBSTR(l.Timestamp,0, 11)) as "Date", 
+SELECT DISTINCT(SUBSTR(l.Timestamp,0, 11)) as "Date", l.Station,
 	"Min Humidity    " as Category, l.Humidity as value
-	FROM year02_1_31 as l JOIN tempT as r on l.humidity = r.minH 
-    	AND SUBSTR(l.Timestamp,0, 8) = r.month
+	FROM SingaporeWeather_1 as l JOIN tempT as r on l.humidity = r.minH
+    	AND SUBSTR(l.Timestamp,0, 8) = r.month AND l.Station = r.Station
 UNION
-SELECT DISTINCT(SUBSTR(l.Timestamp,0, 11)) as "Date", 
+SELECT DISTINCT(SUBSTR(l.Timestamp,0, 11)) as "Date", l.Station,
 	"Max Humidity    " as Category, l.Humidity as value
-	FROM year02_1_31 as l JOIN tempT as r on l.humidity = r.maxH
-    	AND SUBSTR(l.Timestamp,0, 8) = r.month
+	FROM SingaporeWeather_1 as l JOIN tempT as r on l.humidity = r.maxH
+    	AND SUBSTR(l.Timestamp,0, 8) = r.month AND l.Station = r.Station
 UNION
-SELECT DISTINCT(SUBSTR(l.Timestamp,0, 11)) as "Date", 
+SELECT DISTINCT(SUBSTR(l.Timestamp,0, 11)) as "Date", l.Station,
 	"Min Temperature" as Category, l.Temperature as value
-	FROM year02_1_31 as l JOIN tempT as r on l.temperature = r.minT
-    	AND SUBSTR(l.Timestamp,0, 8) = r.month
+	FROM SingaporeWeather_1 as l JOIN tempT as r on l.temperature = r.minT
+    	AND SUBSTR(l.Timestamp,0, 8) = r.month AND l.Station = r.Station
 UNION
-SELECT DISTINCT(SUBSTR(l.Timestamp,0, 11)) as "Date", 
+SELECT DISTINCT(SUBSTR(l.Timestamp,0, 11)) as "Date", l.Station,
 	"Max Temperature" as Category, l.Temperature as value
-	FROM year02_1_31 as l JOIN tempT as r on l.temperature = r.maxT
-    	AND SUBSTR(l.Timestamp,0, 8) = r.month
+	FROM SingaporeWeather_1 as l JOIN tempT as r on l.temperature = r.maxT
+    	AND SUBSTR(l.Timestamp,0, 8) = r.month AND l.Station = r.Station
 ORDER BY 2 DESC;
